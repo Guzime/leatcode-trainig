5deceea7285a7d6df5193e0dce10faac3e72158f
ntolmachev <40675616+Guzime@users.noreply.github.com>
2023-11-01 19:59:25 +0300

    add resolution for task merge two sorted lists

--- src/main/java/org/example/linkedlist/LinkedList4Task.java
+++ src/main/java/org/example/linkedlist/LinkedList4Task.java
@@ -22,10 +22,19 @@ public class LinkedList4Task {
         if (list2 == null) {
             return list1;
         }
+        if (list1.val == list2.val && list1.next == null && list2.next == null) {
+            list2.next = list1;
+            return list2;
+        }
+
         ListNode left = list1;
         ListNode right = list2;
         while (left != null && right != null) {
             ListNode buff;
+            if (left.val == right.val && left.next == null && right.next == null) {
+                right.next = left;
+                break;
+            }
             if (left.val <= right.val) {
                 buff = left.next;
                 if (left.next == null || right.val < left.next.val) {
@@ -39,6 +48,58 @@ public class LinkedList4Task {
                 }
                 right = buff;
             }
+        }
+
+        return list1.val <= list2.val ? list1 : list2;
+    }
+
+    public ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        if (list1.val == list2.val && list1.next == null && list2.next == null) {
+            list2.next = list1;
+            return list2;
+        }
+
+        ListNode left = list1;
+        ListNode right = list2;
+        boolean isLeft = list1.val <= list2.val;
+        while (left != null && right != null) {
+            ListNode buff;
+            if (left.val == right.val) {
+                if (isLeft) {
+                    buff = left.next;
+                    left.next = right;
+                    isLeft = false;
+                    left = buff;
+                } else {
+                    buff = right.next;
+                    right.next = left;
+                    isLeft = true;
+                    right = buff;
+                }
+                continue;
+            }
+            if (left.val < right.val) {
+                buff = left.next;
+                if (left.next == null || right.val < left.next.val) {
+                    left.next = right;
+                    isLeft = false;
+                }
+                left = buff;
+            } else {
+                buff = right.next;
+                if (right.next == null || left.val < right.next.val) {
+                    right.next = left;
+                    isLeft = true;
+                }
+                right = buff;
+            }
+
         }
 
         return list1.val <= list2.val ? list1 : list2;
--- src/test/java/org/example/linkedlist/LinkedList4TaskTest.java
+++ src/test/java/org/example/linkedlist/LinkedList4TaskTest.java
@@ -79,4 +79,179 @@ class LinkedList4TaskTest {
         assertEquals("-9,-7,-7,-7,-6,-3", result.toString());
     }
 
+    @Test
+    public void test2_1() {
+        LinkedList4Task linkedList4Task = new LinkedList4Task();
+        ListNode node1_1 = new ListNode(1);
+
+        ListNode node1_2 = new ListNode(1);
+        ListNode result = linkedList4Task.mergeTwoLists(node1_1, node1_2);
+        assertEquals("1,1", result.toString());
+    }
+
+    @Test
+    public void test2_2() {
+        LinkedList4Task linkedList4Task = new LinkedList4Task();
+        ListNode node1_1 = new ListNode(-9);
+        ListNode node1_2 = new ListNode(-7);
+        ListNode node1_4 = new ListNode(-3);
+        ListNode node1_5 = new ListNode(-3);
+        ListNode node1_6 = new ListNode(-1);
+        ListNode node1_7 = new ListNode(2);
+        ListNode node1_8 = new ListNode(3);
+
+        ListNode node2_1 = new ListNode(-7);
+        ListNode node2_3 = new ListNode(-7);
+        ListNode node2_4 = new ListNode(-6);
+        ListNode node2_5 = new ListNode(-6);
+        ListNode node2_6 = new ListNode(-5);
+        ListNode node2_7 = new ListNode(-3);
+        ListNode node2_8 = new ListNode(2);
+        ListNode node2_9 = new ListNode(4);
+
+        node1_1.next = node1_2;
+        node1_2.next = node1_4;
+        node1_4.next = node1_5;
+        node1_5.next = node1_6;
+        node1_6.next = node1_7;
+        node1_7.next = node1_8;
+
+        node2_1.next = node2_3;
+        node2_3.next = node2_4;
+        node2_4.next = node2_5;
+        node2_5.next = node2_6;
+        node2_6.next = node2_7;
+        node2_7.next = node2_8;
+        node2_8.next = node2_9;
+
+        ListNode result = linkedList4Task.mergeTwoLists2(node1_1, node2_1);
+        assertEquals("-9,-7,-7,-7,-6,-6,-5,-3,-3,-3,-1,2,2,3,4", result.toString());
+    }
+
+    @Test
+    public void test2_3() {
+        LinkedList4Task linkedList4Task = new LinkedList4Task();
+        ListNode node1_1 = new ListNode(1);
+        ListNode node1_2 = new ListNode(2);
+        ListNode node1_4 = new ListNode(4);
+
+        ListNode node2_1 = new ListNode(1);
+        ListNode node2_3 = new ListNode(3);
+        ListNode node2_4 = new ListNode(4);
+
+        node1_1.next = node1_2;
+        node1_2.next = node1_4;
+
+        node2_1.next = node2_3;
+        node2_3.next = node2_4;
+
+        ListNode result = linkedList4Task.mergeTwoLists2(node1_1, node2_1);
+        assertEquals("1,1,2,3,4,4", result.toString());
+    }
+
+    @Test
+    public void test2_4() {
+        LinkedList4Task linkedList4Task = new LinkedList4Task();
+        ListNode node1_1 = new ListNode(1);
+        ListNode node1_2 = new ListNode(2);
+        ListNode node1_4 = new ListNode(3);
+
+        ListNode node2_1 = new ListNode(4);
+        ListNode node2_3 = new ListNode(5);
+        ListNode node2_4 = new ListNode(6);
+
+        node1_1.next = node1_2;
+        node1_2.next = node1_4;
+
+        node2_1.next = node2_3;
+        node2_3.next = node2_4;
+
+        ListNode result = linkedList4Task.mergeTwoLists2(node1_1, node2_1);
+        assertEquals("1,2,3,4,5,6", result.toString());
+    }
+
+    @Test
+    public void test2_5() {
+        LinkedList4Task linkedList4Task = new LinkedList4Task();
+        ListNode node1_1 = new ListNode(1);
+
+
+        ListNode result = linkedList4Task.mergeTwoLists2(node1_1, null);
+        assertEquals("1", result.toString());
+    }
+
+    @Test
+    public void test2_6() {
+        LinkedList4Task linkedList4Task = new LinkedList4Task();
+        ListNode node1_1 = new ListNode(-9);
+        ListNode node1_2 = new ListNode(-7);
+        ListNode node1_4 = new ListNode(-3);
+
+        ListNode node2_1 = new ListNode(-7);
+        ListNode node2_3 = new ListNode(-7);
+        ListNode node2_4 = new ListNode(-6);
+
+        node1_1.next = node1_2;
+        node1_2.next = node1_4;
+
+        node2_1.next = node2_3;
+        node2_3.next = node2_4;
+
+        ListNode result = linkedList4Task.mergeTwoLists2(node1_1, node2_1);
+        assertEquals("-9,-7,-7,-7,-6,-3", result.toString());
+    }
+
+    @Test
+    public void test2_7() {
+        LinkedList4Task linkedList4Task = new LinkedList4Task();
+        ListNode node1_1 = new ListNode(1);
+
+        ListNode node1_2 = new ListNode(1);
+        ListNode result = linkedList4Task.mergeTwoLists2(node1_1, node1_2);
+        assertEquals("1,1", result.toString());
+    }
+
+    @Test
+    public void test2_8() {
+        LinkedList4Task linkedList4Task = new LinkedList4Task();
+        ListNode node1_1 = new ListNode(-6);
+        ListNode node1_2 = new ListNode(-2);
+        ListNode node1_4 = new ListNode(-1);
+        ListNode node1_5 = new ListNode(0);
+        ListNode node1_6 = new ListNode(3);
+        ListNode node1_7 = new ListNode(6);
+        ListNode node1_8 = new ListNode(7);
+        ListNode node1_9 = new ListNode(9);
+
+        ListNode node2_1 = new ListNode(-9);
+        ListNode node2_3 = new ListNode(-7);
+        ListNode node2_4 = new ListNode(-5);
+        ListNode node2_5 = new ListNode(-5);
+        ListNode node2_6 = new ListNode(-1);
+        ListNode node2_7 = new ListNode(0);
+        ListNode node2_8 = new ListNode(0);
+        ListNode node2_9 = new ListNode(5);
+        ListNode node2_10 = new ListNode(9);
+
+        node1_1.next = node1_2;
+        node1_2.next = node1_4;
+        node1_4.next = node1_5;
+        node1_5.next = node1_6;
+        node1_6.next = node1_7;
+        node1_7.next = node1_8;
+        node1_8.next = node1_9;
+
+        node2_1.next = node2_3;
+        node2_3.next = node2_4;
+        node2_4.next = node2_5;
+        node2_5.next = node2_6;
+        node2_6.next = node2_7;
+        node2_7.next = node2_8;
+        node2_8.next = node2_9;
+        node2_9.next = node2_10;
+
+        ListNode result = linkedList4Task.mergeTwoLists2(node1_1, node2_1);
+        assertEquals("-9,-7,-6,-5,-5,-2,-1,-1,0,0,0,3,5,6,7,9,9", result.toString());
+    }
+
 }
